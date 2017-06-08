5d5b0d4706bb8dcf7832a406c543a4d8fadb2991
白俊杰 <625603381@qq.com>
2017-06-08 16:38:31 +0800

    bug fix

--- js/i18n.js
+++ js/i18n.js
@@ -124,75 +124,74 @@
 	}
 
 	function extend() {
-		var options, name, src, copy, copyIsArray, clone,
-			target = arguments[0] || {},
-			targetType = typeof target,
-			toString = Object.prototype.toString,
-			i = 1,
-			length = arguments.length,
-			deep = false;
+		var options, name, src, copy, copyIsArray,
+            target = arguments[0] || {},
+            targetType = typeof target,
+            toString = Object.prototype.toString,
+            i = 1,
+            length = arguments.length,
+            deep = false;
 
-		// 处理深拷贝
-		if (targetType === 'boolean') {
-			deep = target;
+        // 处理深拷贝
+        if (targetType === 'boolean') {
+            deep = target;
 
-			// Skip the boolean and the target
-			target = arguments[i] || {};
-			targetType = typeof target;
-			i++;
-		}
+            // Skip the boolean and the target
+            target = arguments[i] || {};
+            targetType = typeof target;
+            i++;
+        }
 
-		// Handle case when target is a string or something (possible in deep copy)
-		if (targetType !== 'object' && targetType !== 'function') {
-			target = {};
-		}
+        // Handle case when target is a string or something (possible in deep copy)
+        if (targetType !== 'object' && targetType !== 'function') {
+            target = {};
+        }
 
-		// 如果没有合并的对象，则表示 target 为合并对象，将 target 合并给当前函数的持有者
-		if (i === length) {
-			target = this;
-			i--;
-		}
+        // 如果没有合并的对象，则表示 target 为合并对象，将 target 合并给当前函数的持有者
+        if (i === length) {
+            target = this;
+            i--;
+        }
 
-		for (; i < length; i++) {
+        for (; i < length; i++) {
 
-			// Only deal with non-null/undefined values
-			if ((options = arguments[i]) != null) {
+            // Only deal with non-null/undefined values
+            if ((options = arguments[i]) != null) {
 
-				// Extend the base object
-				for (name in options) {
-					src = target[name];
-					copy = options[name];
+                // Extend the base object
+                for (name in options) {
+                    src = target[name];
+                    copy = options[name];
 
-					// 防止死循环
-					if (target === copy) {
-						continue;
-					}
+                    // 防止死循环
+                    if (target === copy) {
+                        continue;
+                    }
 
-					// 深拷贝对象或者数组
-					if (deep && copy &&
-						(copyIsArray = toString.call(copy) === '[object Array]') ||
-						(toString.call(copy) === '[object Object]')) {
+                    // 深拷贝对象或者数组
+                    if (deep && copy &&
+                        ((copyIsArray = toString.call(copy) === '[object Array]') ||
+                        (toString.call(copy) === '[object Object]'))) {
 
-						if (copyIsArray) {
-							copyIsArray = false;
-							src = src && (toString.call(src) === '[object Array]') ? src : [];
+                        if (copyIsArray) {
+                            copyIsArray = false;
+                            src = src && (toString.call(src) === '[object Array]') ? src : [];
 
-						} else {
-							src = src && (toString.call(src) === '[object Object]') ? src : {};
-						}
+                        } else {
+                            src = src && (toString.call(src) === '[object Object]') ? src : {};
+                        }
 
-						target[name] = extend(deep, src, copy);
+                        target[name] = extend(deep, src, copy);
 
+                    } else if (copy !== undefined) { // 仅忽略未定义的值
+                        target[name] = copy;
+                    }
+                }
+            }
+        }
 
-					} else if (copy !== undefined) { // 仅忽略未定义的值
-						target[name] = copy;
-					}
-				}
-			}
-		}
-
-		// Return the modified object
-		return target;
+        // Return the modified object
+        return target;
 	}
 
 	// 执行指定事件类型的 callback
